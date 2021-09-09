dbe3363b5c6be3134db5fb97567cc12ac823733d
Sadayoshi Tada <tadainfra@gmail.com>
2021-09-10 01:59:30 +0900

    Constantsのコードを写経

---
+++ go-sandbox/gobyexample/Constants/constant.go
@@ -0,0 +1,22 @@
+// const は定数
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "constant"
+
+func main() {
+	fmt.Println(s)
+
+	const n = 500000000
+
+	const d = 3e20 / n
+	fmt.Println(n)
+
+	fmt.Println(int64(d))
+
+	fmt.Println(math.Sin(n))
+}
