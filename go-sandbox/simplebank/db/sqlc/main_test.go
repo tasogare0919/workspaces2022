a26f659365834860fd8532916892236f84cc3520
Sadayoshi Tada <tadainfra@gmail.com>
2022-02-19 20:49:08 +0900

    sqlc generate code add

--- go-sandbox/simplebank/db/sqlc/main_test.go
+++ go-sandbox/simplebank/db/sqlc/main_test.go
@@ -19,7 +19,7 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(iver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
