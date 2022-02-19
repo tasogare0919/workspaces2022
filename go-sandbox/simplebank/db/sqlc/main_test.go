5f60d5ebf527ca273239aa1940503e687479db06
Sadayoshi Tada <tadainfra@gmail.com>
2022-02-19 21:13:59 +0900

    config setting modify

--- go-sandbox/simplebank/db/sqlc/main_test.go
+++ go-sandbox/simplebank/db/sqlc/main_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
+	"github.com/techschool/simplebank/util"
 )
 
 const (
@@ -19,7 +20,11 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
