cf70da8f6f33e7a6bcea70b4b3fefc3b7daafb1b
Sadayoshi Tada <tadainfra@gmail.com>
2022-02-19 20:52:38 +0900

    typo modify

--- go-sandbox/simplebank/db/sqlc/main_test.go
+++ go-sandbox/simplebank/db/sqlc/main_test.go
@@ -19,7 +19,7 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open(iver, dbSource)
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
